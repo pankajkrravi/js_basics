b1420a0e353d796ce9f56db8736eeffdf3eda9b4
Pankaj <pankaj.ravi@goomo.com>
2019-09-29 11:40:53 +0530

    Basics Js programs

---
+++ first.js
@@ -0,0 +1,4 @@
+let name='pankaj kumar ravi';
+console.log(name);
+let cons=99.0;
+console.log(cons);
---
+++ varcons.js
@@ -0,0 +1,6 @@
+let name='pankaj';
+let age=25;
+let iscal=true;
+let lastname;//by default is undefined
+let selectedcolor=null;
+//list iof promitive data types
